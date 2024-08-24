cc6dc70bf8d13297b0ff10beee9acd75e559ecd1
vinod192954 <vvinnu590@gmail.com>
2024-08-24 17:59:34 +0530

    added api to perform crud operations

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const { error } = require('console')
+const { create } = require('domain')
 const express = require('express')
 const path= require('path')
 const {open} = require('sqlite')
@@ -5,7 +7,7 @@ const sqlite3 = require('sqlite3')
 const app=express()
 const dbPath = path.join(__dirname,'blogsData.db')
 let db = null
-
+app.use(express.json())
 
 const initializeDBAndServer =async ()=>{
    try{
@@ -50,7 +52,72 @@ app.get('/detailedPosts/:blogId',async(request,response)=>{
     
 })
 
+app.post('/posts',async(request,response)=>{
+    const {title,excerpt,content,author,publish_date,detailed_content} = request.body 
+    const blogPostQuery = `INSERT INTO  blog_posts (title,excerpt,content) VALUES (?,?,?)` 
+    const createBlogDetailsQuery = `INSERT INTO detailed_blog(blog_id,author,publish_date,detailed_content) VALUES(?,?,?,?) `
+    try{
+    await db.exec("BEGIN TRANSACTION")
+    const blogPostResult = await db.run(blogPostQuery,[title,excerpt,content])
+    const blogId = blogPostResult.lastID 
+    await db.run(createBlogDetailsQuery,[blogId,author,publish_date,detailed_content])
+    await db.exec("COMMIT")
+    response.status(201).send({message:"Blog post and details are created successfully"})
+ }catch(error){
+    await db.exec("ROLLBACK")
+    response.status(500).send({ error: "Database error", details: error.message });
+ }
+
+   
+
+})  
 
 
+app.put("/posts/:blogId",async(request,response)=>{
+    const {blogId} = request.params 
+    const {title,excerpt,content} = request.body 
+    try{
 
+ const updateBlogQuery = 
+    `UPDATE blog_posts 
+    SET  
+       title = ?,
+       excerpt = ?,
+       content = ? 
+    WHERE 
+       id = ? ` 
+    const dbResponse = await db.run(updateBlogQuery,[title,excerpt,content,blogId])
+    if (dbResponse.changes!==0){
+        response.send({message:"Blog post updated successfully"})
+    }
+    else{
+        response.status(404).send({message:"Blog post not found"})
+    }
+
+    }catch(error){
+        response.status(500).send({error:'Database error',details:error.message})
+    }
+    
+})
+
+app.delete("/posts/:blogId",async(request,response)=>{
+    const {blogId} = request.params 
+    try{
+
+        const deleteBlogQuer = `DELETE FROM blog_posts WHERE id = ?` 
+        const dbResponse = await db.run(deleteBlogQuer,[blogId])
+         
+        if (dbResponse.changes!==0){
+         response.status(200).send({message:"Blog post deleted successfully"})
+        }
+        else{
+         response.status(404).send({message:"Blog post not found"})
+        }
+
+    }catch{
+        response.status(500).send({error:"Database error",details:error.message})
+    }
+   
+
+})
 
